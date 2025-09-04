216b29da9041c3d63fb5779e097d6d3c5e522bbd
Paul Leflon <leflon.paul@gmail.com>
2025-09-04 15:54:31 +0200

    feat(fetch): fetch data from Seoul Metro's CyberStation page.

--- fetch.ts
+++ fetch.ts
@@ -1,203 +1,75 @@
 import Database from 'better-sqlite3';
-import { createReadStream, statSync, writeFileSync } from 'fs';
+import { readFileSync, writeFileSync } from 'fs';
+import * as cheerio from 'cheerio';
 import { plainify } from './src/lib/utils.ts';
 
-const db = new Database('data/idfm.db');
+const db = new Database('data/jihacheol.db');
 db.exec('PRAGMA journal_mode = WAL');
 
-/* Datasets */
-const STOPS_URL =
-	'https://data.iledefrance-mobilites.fr/api/explore/v2.1/catalog/datasets/emplacement-des-gares-idf-data-generalisee/exports/json';
-const LINES_URL =
-	'https://data.iledefrance-mobilites.fr/api/explore/v2.1/catalog/datasets/referentiel-des-lignes/exports/json';
-const STOP_DETAILS_URL =
-	'https://data.iledefrance-mobilites.fr/api/explore/v2.1/catalog/datasets/arrets/exports/json';
-const MAP_URL =
-	'https://data.iledefrance-mobilites.fr/api/explore/v2.1/catalog/datasets/traces-du-reseau-ferre-idf/exports/geojson';
-
-/* CLI flags */
-const LOAD_LOCAL = process.argv.includes('--local'); // Load from local save rather than distant dataset
-const WIPE_CURRENT = process.argv.includes('--wipe'); // Wipe existing database
-
-console.log('== Initializing database ==');
-if (WIPE_CURRENT) {
-	db.exec('DROP TABLE IF EXISTS StopModes');
-	db.exec('DROP TABLE IF EXISTS Stops');
-	db.exec('DROP TABLE IF EXISTS Lines');
-}
-db.exec(
-	'CREATE TABLE IF NOT EXISTS Stops(id TEXT PRIMARY KEY, name TEXT, plain_name TEXT, lines TEXT, town TEXT, fare_zone TEXT, geo TEXT)'
-);
-db.exec(
-	'CREATE TABLE IF NOT EXISTS Lines(name TEXT PRIMARY KEY, mode TEXT, picto TEXT, color TEXT, text_color)'
-);
-// Storing modes (metro, rer, etc.) in a separate table allows to filter stations by mode efficiently.
-db.exec('CREATE TABLE IF NOT EXISTS StopModes(stop_id TEXT, mode TEXT)');
-db.exec('CREATE INDEX IF NOT EXISTS idx_stops_plain_name ON Stops(plain_name)'); // For search lookup
-db.exec('CREATE INDEX IF NOT EXISTS idx_stop_modes ON StopModes(mode)'); // For filtering
-
-console.log('== Fetching data ==');
-type Datapoint = Record<string, string | Object | number>;
-type Dataset = Datapoint[];
-let stops: Dataset, lines: Dataset, stopDetails: Dataset, map: Dataset;
-
-/**
- * Fetches data from a URL or local file with a progress indicator.
- * @param url The URL or local file path.
- * @returns The parsed JSON data.
- */
-const fetchWithProgress = async (url: string): Promise<Dataset> => {
-	let chunks: (Buffer | Uint8Array)[] = [];
-	let receivedLength = 0;
-	let contentSize = 0;
-
-	if (url.startsWith('http')) {
-		const response = await fetch(url);
-		const reader = response.body!.getReader();
-		contentSize = Number(response.headers.get('Content-Length')) || 0;
-
-		while (true) {
-			const { done, value } = await reader.read();
-			if (done) {
-				break;
-			}
-			chunks.push(value);
-			receivedLength += value.length;
-			process.stdout.write(
-				`\rReceived ${(receivedLength / (1024 * 1024)).toFixed(2)} MB${contentSize === 0 ? '' : ` of ${(contentSize / (1024 * 1024)).toFixed(2)} MB`}`
-			);
-		}
-	} else {
-		// Using a promise-based approach with Node.js streams for local files
-		const stream = createReadStream(url);
-		const { size } = statSync(url);
-		contentSize = size;
-
-		await new Promise((resolve, reject) => {
-			stream.on('data', (chunk) => {
-				chunks.push(chunk as Buffer);
-				receivedLength += chunk.length;
-				process.stdout.write(
-					`\rReceived ${(receivedLength / (1024 * 1024)).toFixed(2)} MB${contentSize === 0 ? '' : ` of ${(contentSize / (1024 * 1024)).toFixed(2)} MB`}`
-				);
-			});
-			stream.on('end', () => {
-				resolve(void 0); // cool
-			});
-			stream.on('error', (err) => {
-				reject(err);
-			});
-		});
-	}
-
-	const chunksAll = Buffer.concat(chunks as any);
-	const result = chunksAll.toString('utf-8');
-	console.log();
-	return JSON.parse(result);
+// We will use more comprehensive line IDs in database.
+const SVG_ID_TABLE: Record<string, string> = {
+	// 왜 그러신건가요?
+	'2-1': '1',
+	'3-2': '2',
+	'4-3': '3',
+	'5-4': '4',
+	'6-5': '5',
+	'7-6': '6',
+	'8-7': '7',
+	'9-8': '8',
+	'10-9': '9',
+	// I1 하지
+	I: 'incheon1',
+	I2: 'incheon2',
+	B: 'suinbundang',
+	S: 'sinbundang',
+	K: 'gyeonguijungang',
+	A: 'arex',
+	G: 'gyeongchun',
+	U: 'uijeongbu',
+	E: 'everline',
+	KK: 'gyeongang',
+	W: 'uilrt',
+	SH: 'seohae',
+	KP: 'gimpo',
+	SL: 'sillim',
+	'1-GA': 'gtx-a'
 };
 
-console.log('Fetching stops...');
-stops = await fetchWithProgress(LOAD_LOCAL ? 'data/stops.json' : STOPS_URL);
-console.log('Fetching lines...');
-lines = await fetchWithProgress(LOAD_LOCAL ? 'data/lines.json' : LINES_URL);
-console.log('Fetching stop details...');
-stopDetails = await fetchWithProgress(LOAD_LOCAL ? 'data/stop_details.json' : STOP_DETAILS_URL);
+db.exec('CREATE TABLE IF NOT EXISTS Lines(id TEXT PRIMARY KEY, name TEXT)');
+db.exec(
+	'CREATE TABLE IF NOT EXISTS Stops(id TEXT PRIMARY KEY, name TEXT, plain_name TEXT, lines TEXT)'
+);
 
-if (!LOAD_LOCAL) {
-	writeFileSync('data/stops.json', JSON.stringify(stops));
-	writeFileSync('data/lines.json', JSON.stringify(lines));
-	writeFileSync('data/stop_details.json', JSON.stringify(stopDetails));
-	// This dataset is not to be stored in database, we only need the JSON file.
-	console.log('Fetching map...');
-	map = await fetchWithProgress(MAP_URL);
-	writeFileSync('data/map.json', JSON.stringify(map));
-}
+const cyberStationHTML = readFileSync('cyberStation.html', { encoding: 'utf-8' });
+const $ = cheerio.load(cyberStationHTML);
 
-console.log('== Formatting and saving... (Might take some time)');
+const stops = $('svg text');
 
-const insertStop = db.prepare('INSERT OR IGNORE INTO Stops VALUES (?, ?, ?, ?, ?, ?, ?)');
-const insertStopMode = db.prepare('INSERT OR IGNORE INTO StopModes VALUES (?, ?)');
-const insertLine = db.prepare('INSERT OR IGNORE INTO Lines VALUES(?, ?, ?, ?, ?)');
+const addStop = db.prepare('INSERT OR IGNORE INTO Stops VALUES (?,?,?,?)');
 
-const LINES_REGEX = /(\w+) (\w+)(?: \/)?/gi;
-/**
- * Parses lines names from dataset
- * @param linesString The raw `res_com` provided by IDFM
- */
-function parseLines(linesString: string): string[] {
-	let result: string[] = [];
-	let parsed: RegExpExecArray | null;
-	while ((parsed = LINES_REGEX.exec(linesString)) !== null) {
-		let line = parsed[2];
-		if (parsed[1] === 'TRAM') line = `T${line}`;
-		if (line.includes('bis')) line = line.replace('bis', 'B');
-		if (line.includes('MONTMARTRE')) line = 'FUNICULAIRE';
-		line.replaceAll(' ', '');
-		result.push(line);
-	}
-	if (result.length === 0) result = [linesString];
-	return result;
-}
-
-/**
- * Parses mode from string, taking care of edge cases.
- * @param mode the raw mode provided by IDFM
- */
-function parseMode(mode: string): string {
-	mode = mode.toLowerCase();
-	if (mode === 'funicular') mode = 'metro'; // including funicular in metro to include it in games as *somewhat* an easter egg.
-	if (mode === 'rail' || mode === 'transilien' || mode === 'val') mode = 'train';
-	if (mode === 'tramway') mode = 'tram';
-	return mode;
-}
-
-const insertData = db.transaction((stops) => {
-	stops.map((stop: Datapoint) => {
-		let details = stopDetails.find(
-			(s) => s.zdaid === String(stop.id_ref_zda) && s.arrfarezone !== null
-		);
-		if (!details) {
-			// Stop is out of Ile-de-France, fetch without farezone constraint
-			// and set it manually.
-			details = stopDetails.find((s) => s.zdaid === String(stop.id_ref_zda));
-			if (!details) return console.log('No details for', stop.nom_zda, stop.res_com);
-			details.arrfarezone = 'Hors IDF';
-		}
-		if (stop.res_com === 'TER') return; // Ignoring TER-only stops
-		const plainName = plainify(stop.nom_long as string);
-		const stopLines = parseLines(stop.res_com as string);
-		const modes = stop.mode as string;
-		const geo = `${(stop.geo_point_2d as any).lon},${(stop.geo_point_2d as any).lat}`;
-		insertStop.run(
-			String(stop.id_ref_zda),
-			stop.nom_long,
-			plainName,
-			stopLines.join(','),
-			details.arrtown,
-			details.arrfarezone,
-			geo
-		);
-
-		for (const mode of modes.split(' / ')) {
-			insertStopMode.run(String(stop.id_ref_zda), parseMode(mode));
-		}
-
-		for (const id of (stop.idrefligc as string).split(' / ')) {
-			const line = lines.find((l) => l.id_line === id);
-			if (!line) continue;
-			let mode = line.networkname ? (line.networkname as string) : (line.transportmode as string);
-			mode = parseMode(mode);
-			insertLine.run(
-				(line.name_line as string).replaceAll(' ', ''),
-				mode,
-				(line.picto as any)?.url,
-				line.colourweb_hexa,
-				line.textcolourweb_hexa
-			);
-		}
+stops.each(function () {
+	const elm = $(this);
+	const id = elm.attr('id');
+	const linesRaw = elm.attr('lineid');
+	if (!id || !linesRaw) return;
+	const lines = linesRaw.split(' ').map((l) => SVG_ID_TABLE[l]);
+	let name = '';
+	elm.children('tspan').each(function () {
+		name += $(this).text().trim();
 	});
+	const plainName = plainify(name);
+	addStop.run(id, name, plainName, lines.join(','));
 });
 
-insertData(stops);
+// SVG MAP SAVE;
 
-console.log('Done');
+const mapStr = $('svg').toString();
+
+const map = cheerio.load(mapStr, { xml: true });
+map('text').remove();
+map('path, rect, g, line, circle').each(function () {
+	if (map(this).attr('id')?.startsWith('A')) return map(this).remove();
+	map(this).attr('style', 'opacity: 1');
+});
+writeFileSync('src/assets/map.svg', `<svg>${map('svg').toString()}</svg>`);
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,9 +1,33 @@
+export const LINE_NAMES_BY_ID = {
+	'1': '1호선',
+	'2': '2호선',
+	'3': '3호선',
+	'4': '4호선',
+	'5': '5호선',
+	'6': '6호선',
+	'7': '7호선',
+	'8': '8호선',
+	'9': '9호선',
+	'10': '10호선',
+	incheon1: '인천1호선',
+	incheon2: '인천2호선',
+	suinbundang: '수인분당선',
+	sinbundang: '신분당',
+	gyeonguijungang: '경의중앙',
+	arex: '공항철도',
+	gyeongchun: '경춘선',
+	uijeongbu: '의정부경전철',
+	everline: '용인경전철',
+	gyeongang: '경강선',
+	uilrt: '우이신설선경전철',
+	seohae: '서해선',
+	gimpo: '김포도시철도',
+	sillim: '신림선',
+	'gtx-a': 'GTX-A'
+};
+
 export function plainify(str: string): string {
-	return str
-		.toLowerCase()
-		.replace(/[ \-\(\)\[\]']/g, '')
-		.normalize('NFD')
-		.replace(/[\u0300-\u036f]/g, '');
+	return str.replace(/[ \-\(\)\[\]']/g, '');
 }
 
 export function debounce<T extends (...args: any[]) => void>(
